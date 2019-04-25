d0634bf5a2617b4ab71659acc7956cebb59eb998
Mads Mikkel Rasmussen <mara@aspit.dk>
2019-04-25 13:23:05 +0200

    Added Repository class to DataAccess.

---
+++ Northwind.DataAccess/Repository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess
+{
+    /// <summary>
+    /// Represents the data source.
+    /// </summary>
+    public class Repository
+    {
+        #region Fields and constants
+        const string connectionStringName = "NorthwindDB";
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of Repository. Attempts to establish a connection, and will throw an exception on connection error.
+        /// </summary>
+        public Repository()
+        {
+            try
+            {
+                SqlConnection connection = GetConnection(connectionStringName) as SqlConnection;
+                (bool, Exception) connectionAttemptResult = TryConnectUsing(connection);
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Data access error. See inner exception for details", e);
+            }
+        }
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Executes the provided SQL statement and returns data wrapped in a data set, if any.
+        /// </summary>
+        /// <param name="sql">The SQL statement to execute.</param>
+        /// <returns>A <see cref="DataSet"/> wrapping any returned data.</returns>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref=""
+        public DataSet Execute(string sql)
+        {
+            if(String.IsNullOrWhiteSpace(sql))
+            {
+                throw new ArgumentException("Null or whitespace.");
+            }
+            DataSet resultSet = new DataSet();
+            try
+            {
+                SqlConnection connection = GetConnection(connectionStringName) as SqlConnection;
+                using(SqlDataAdapter adapter = new SqlDataAdapter(new SqlCommand(sql, connection)))
+                {
+                    adapter.Fill(resultSet);
+                }
+                return resultSet;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Data access error. See inner exception for details", e);
+            }
+        }
+
+        /// <summary>
+        /// Creates a connection based on the name of a connection string that is stored in a config file.
+        /// </summary>
+        /// <param name="name">The name of the connection string.</param>
+        /// <returns>A new connection.</returns>
+        private static DbConnection GetConnection(string name)
+        {
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
+            DbProviderFactory factory = DbProviderFactories.GetFactory(settings.ProviderName);
+            DbConnection connection = factory.CreateConnection();
+            connection.ConnectionString = settings.ConnectionString;
+            return connection;
+        }
+
+        /// <summary>
+        /// Attempts to connect to a data source using the provided connection.
+        /// </summary>
+        /// <param name="connection">The connection to use.</param>
+        /// <returns>True, if the connection could be established, false otherwise.</returns>
+        public (bool, Exception) TryConnectUsing(DbConnection connection)
+        {
+            try
+            {
+                using(connection)
+                {
+                    connection.Open();
+                    connection.Close();
+                }
+                return (true, null);
+            }
+            catch(Exception e)
+            {
+                return (false, e);
+            }
+        }
+        #endregion
+    }
+}
