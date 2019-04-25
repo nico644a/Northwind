0222f29c8d14d39567cb63ad7fe43d18448ff907
Mads Mikkel Rasmussen <mara@aspit.dk>
2019-04-25 13:33:11 +0200

    Added missing provider name in app confogs. Added unit test for repository.

---
+++ Northwind.Tests/Repositorytests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Northwind.DataAccess;
+
+namespace Northwind.Tests
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void RepositoryInitializationSucceeds()
+        {
+            // Arrange:
+            Repository repository;
+            
+            // Act:
+            repository = new Repository();
+        }
+    }
+}
