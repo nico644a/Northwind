8e5b30736662508474dc07973b60f9ff03d58249
Mads Mikkel Rasmussen <mara@aspit.dk>
2019-05-01 12:54:32 +0200

    Completed unit tests for validation.

--- Northwind.Tests/ValidationsTests.cs
+++ Northwind.Tests/ValidationsTests.cs
@@ -14,13 +14,65 @@ namespace Northwind.Tests
         [TestMethod()]
         public void TextOnlyTest()
         {
-            Assert.Fail();
+            // Arrange:
+            string text = "Mads";
+            string nullText = null;
+            string empty = String.Empty;
+            string numbers = "1234";
+            string otherChars = "!#¤%";
+            string textWithNumber = "Mad5";
+            string textWithOtherChar = "M@ds";
+
+            // Act:
+            bool validationResultText = Validations.TextOnly(text);
+            bool validationResultNull = Validations.TextOnly(nullText);
+            bool validationResultEmpty = Validations.TextOnly(empty);
+            bool validationResultNumbers = Validations.TextOnly(numbers);
+            bool validationResultOtherChars = Validations.TextOnly(otherChars);
+            bool validationResultTextWithNumber = Validations.TextOnly(textWithNumber);
+            bool validationResultTextWithOtherChar = Validations.TextOnly(textWithOtherChar);
+
+            // Assert:
+            Assert.IsTrue(validationResultText);
+            Assert.IsFalse(validationResultNull);
+            Assert.IsFalse(validationResultEmpty);
+            Assert.IsFalse(validationResultNumbers);
+            Assert.IsFalse(validationResultOtherChars);
+            Assert.IsFalse(validationResultTextWithNumber);
+            Assert.IsFalse(validationResultTextWithOtherChar);
         }
 
         [TestMethod()]
         public void TextOnlySentenceTest()
         {
-            Assert.Fail();
+            // Arrange:
+            string sentence = "Mads Mikkel Rasmussen";
+            string nullSentence = null;
+            string empty = String.Empty;
+            string untrimmedSentence = " " + sentence + " ";
+
+            // Act:
+            bool validationResultSentence = Validations.TextOnlySentence(sentence);
+            bool validationResultNull = Validations.TextOnlySentence(nullSentence);
+            bool validationResultEmpty = Validations.TextOnlySentence(empty);
+            bool validationResultUntrimmed = Validations.TextOnlySentence(untrimmedSentence);
+
+
+            // Assert:
+            Assert.IsTrue(validationResultSentence);
+            Assert.IsFalse(validationResultNull);
+            Assert.IsFalse(validationResultEmpty);
+            Assert.IsFalse(validationResultUntrimmed);
+
+            validationResultSentence = Validations.TextOnlySentence2(sentence);
+            validationResultNull = Validations.TextOnlySentence2(nullSentence);
+            validationResultEmpty = Validations.TextOnlySentence2(empty);
+            validationResultUntrimmed = Validations.TextOnlySentence2(untrimmedSentence);
+
+            Assert.IsTrue(validationResultSentence);
+            Assert.IsFalse(validationResultNull);
+            Assert.IsFalse(validationResultEmpty);
+            Assert.IsFalse(validationResultUntrimmed);
         }
 
         [TestMethod()]
--- Northwind.Utils/Validations.cs
+++ Northwind.Utils/Validations.cs
@@ -10,14 +10,14 @@ namespace Northwind.Utils
     {
         public static bool TextOnly(string s)
         {
-            if(s is null)
+            if(string.IsNullOrWhiteSpace(s))
                 return false;
             return s.All(c => Char.IsLetter(c));
         }
 
         public static bool TextOnlySentence(string s)
         {
-            if(s is null)
+            if(string.IsNullOrWhiteSpace(s))
                 return false;
             foreach(string word in s.Split(' ', '\t'))
                 if(!TextOnly(word))
@@ -25,7 +25,10 @@ namespace Northwind.Utils
             return true;
         }
 
+        
+        // Demonstrates the function like syntax for a method, and the ternary decision operator ? : and the LINQ ext. All() applied to an IEnummerable (in this case a String array).
+        // Same result as above, just with a different and more compact syntax.
         public static bool TextOnlySentence2(string s)
-            => s.Split(' ', '\t').All(word => TextOnly(word));
+            => String.IsNullOrWhiteSpace(s) ? false : s.Split(' ', '\t').All(word => TextOnly(word));
     }
 }
